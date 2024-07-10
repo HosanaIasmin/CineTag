ae5104418096250c3f942b257ac464b6149e53a4
Leonacs <hosana.castro048@gmail.com>
2024-07-10 09:07:35 -0300

    fix: ajustando botão de favorito

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,6 @@ function Card({id, titulo, capa}) {
         <div className={styles.conatiner}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar" className={styles.favoritar} />
         </div>
     )
 }
